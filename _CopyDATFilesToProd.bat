1791fc68d1c9acb317c8845697a5c7833fb4b6ac
vzhomeexperiments <vz.home.experiments@gmail.com>
2018-11-20 20:59:31 +0100

    renamed bat file, adapted FALCON A code to work with T3 and T4

--- _CopyDATFilesToProd.bat
+++ _CopyDATFilesToProd.bat
@@ -9,9 +9,11 @@ set SOURCE_DIR="C:\Program Files (x86)\FxPro - Terminal2\tester\files"
 :: files copied to the sandbox folder MQL4\Files
 set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Files"
 set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Files"
+set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Files"
 
 rem only copy *.dat files
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% *.dat
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% *.dat
+ROBOCOPY %SOURCE_DIR% %DEST_DIR3% *.dat
 
 pause
