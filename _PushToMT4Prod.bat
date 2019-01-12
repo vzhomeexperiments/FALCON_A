7e482cbe3e2aa0adce366f89bd1e7133f610ee00
surapoom <45107078+surapoom@users.noreply.github.com>
2019-01-13 00:44:33 +0700

    Adapt to local

--- _PushToMT4Prod.bat
+++ _PushToMT4Prod.bat
@@ -4,11 +4,11 @@ rem Use when you need to publish all files to All Terminals
 @echo off
 setlocal enabledelayedexpansion
 
-set SOURCE_DIR="C:\Users\fxtrams\Documents\000_TradingRepo\FALCON_A"
-set DEST_DIR1="C:\Program Files (x86)\FxPro - Terminal1\MQL4\Experts\03_FALCON_A"
-set DEST_DIR2="C:\Program Files (x86)\FxPro - Terminal2\MQL4\Experts\03_FALCON_A"
-set DEST_DIR3="C:\Program Files (x86)\FxPro - Terminal3\MQL4\Experts\03_FALCON_A"
-set DEST_DIR4="C:\Program Files (x86)\FxPro - Terminal4\MQL4\Experts\03_FALCON_A"
+set SOURCE_DIR="C:\LazyTrading\GitHub\FALCON_A"
+set DEST_DIR1="C:\Program Files (x86)\ICMarkets MT4 Terminal1\MQL4\Experts\03_FALCON_A"
+set DEST_DIR2="C:\Program Files (x86)\ICMarkets MT4 Terminal2\MQL4\Experts\03_FALCON_A"
+set DEST_DIR3="C:\Program Files (x86)\ICMarkets MT4 Terminal3\MQL4\Experts\03_FALCON_A"
+set DEST_DIR4="C:\Program Files (x86)\ICMarkets MT4 Terminal4\MQL4\Experts\03_FALCON_A"
 
 ROBOCOPY %SOURCE_DIR% %DEST_DIR1% Falcon_A.mq4
 ROBOCOPY %SOURCE_DIR% %DEST_DIR2% Falcon_A.mq4
